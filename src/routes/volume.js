8fb7a14737b46f6422035b1ab97f6a06cc758068
Hugo-NF <hugonfonseca@hotmail.com>
2020-06-27 15:36:30 -0300

    Linting volume routes

--- src/routes/volume.js
+++ src/routes/volume.js
@@ -1,6 +1,6 @@
 /**
  * This file contains all the routes related to the volume entity
- * 
+ *
  */
 
 const router = require('express').Router();
@@ -19,6 +19,5 @@ router.post('/create', celebrate(createValidation), VolumesController.create);
 router.get('/:id', VolumesController.show);
 router.put('/:id', celebrate(editValidation), VolumesController.edit);
 router.delete('/:id', celebrate(deleteValidation), VolumesController.delete);
-
 
 module.exports = router;
