d53e61ce63066214ff3951b978bc9a6c7a3cf5b8
Ailamar Alves <ailamar.alvesg@gmail.com>
2020-07-03 18:13:57 -0300

    add health route

--- src/models/Health.js
+++ src/models/Health.js
@@ -1,20 +1,37 @@
 const mongoose = require('mongoose');
 
 const healthSchema = new mongoose.Schema({
-  date: { type: String },
-  measures: [{
-    hight: { type: Number  },
-    weight: { type: Number },
-    body: { type: Number },
-    body: { type: Number },
-    body: { type: Number },
+  date: {
+    type: Date,
+    required: true,
+  },
+  measures: [
+    {
+    hight: { type: String, required: true },
+    weight: { type: Boolean, required: true },
+    chest: { type: Boolean, required: true },     // torax
+    waist: { type: Boolean, required: true },     // cintura
+    abdomen: { type: Boolean, required: true },   // abdomen
+    hip: { type: Boolean, required: true },       // quadril
+    forearm: { type: Boolean, required: true },   // antebraço
+    arm: { type: Boolean, required: true },       // braço
+    thigh: { type: Boolean, required: true },     // coxa
+    calf: { type: Boolean, required: true },      // panturrilha
+    },
+  ],  
+  bodyComposition: [{
+    IMC: { type: Number, required: true },
+    fatPercentage: { type: Number, required: true },
+    IAC: { type: Number, required: true },    
   }],
-  indexes: [{
-    IMC: { type: Number },
-    fatPercentage: { type: Number },
-    IAC: { type: Number },    
-  }],
-
+  vo2max: {
+    type: Number,
+    required: true,
+  },
+  objective: {
+    type: String,
+    required: true,
+  },
 },
 {
   timestamps: true,
