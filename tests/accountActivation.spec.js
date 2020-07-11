d70bf0446111d6ec270ec23cb7f103956642cb3e
Paulo Victor de Menezes Lopes <pv.paulovictor.vp@gmail.com>
2020-07-10 23:10:52 -0300

    Add more User tests

--- tests/accountActivation.spec.js
+++ tests/accountActivation.spec.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const app = require('../src/app');
+const logger = require('../src/config/configLogging')
 const errors = require('../src/config/errorsEnum');
 
 const User = require('../src/models/User');
@@ -73,6 +74,21 @@ describe('Account Activation', () => {
       .get(`/api/users/activate/${user.emailConfirmationToken}?sandboxMode=true`);
 
     expect(response.status).toBe(403);
+
+    done();
+  });
+
+  it('should not activate account successfully', async (done) => {
+    // Request e-mail to be sent
+    let response = await request(app)
+      .post('/api/users/activate')
+      .send({
+        email: 'hugonfonseca.wrong@hotmail.com',
+        sandboxMode: true, // Prevents the email dispatch, thus preventing consumption while testing
+      });
+
+    expect(response.status).toBe(409);
+    expect(response.body.errorCode).toBe(errors.USER_NOT_IN_DATABASE);
 
     done();
   });
--- tests/resetPassword.spec.js
+++ tests/resetPassword.spec.js
@@ -97,4 +97,72 @@ describe('Reset Password', () => {
 
     done();
   });
+
+  it('should check reset password token successfully', async (done) => {
+    // Request password reset e-mail
+    let response = await request(app)
+      .post('/api/users/password/reset')
+      .send({
+        email: 'hugoresetpw@hotmail.com',
+        sandboxMode: true,
+      });
+
+    expect(response.status).toBe(200);
+
+    const user = await User.findOne({ email: 'hugoresetpw@hotmail.com' });
+    response = await request(app)
+      .get(`/api/users/password/reset/${user.resetPasswordToken}`);
+    
+    expect(response.status).toBe(200);
+    expect(response.body.data).toHaveProperty('user');
+
+    done();
+  });
+
+  it('should check invalid reset password token', async (done) => {
+    response = await request(app)
+      .get(`/api/users/password/reset/wrong`);
+    
+    expect(response.status).toBe(409);
+    expect(response.body.errorCode).toBe(errors.TOKEN_NOT_GENERATED);
+
+    done();
+  });
+
+  it('should check expired token', async (done) => {
+    // Request password reset e-mail
+    let response = await request(app)
+      .post('/api/users/password/reset')
+      .send({
+        email: 'hugoresetpwtwice@hotmail.com',
+        sandboxMode: true,
+      });
+
+    expect(response.status).toBe(200);
+
+    const user = await User.findOne({ email: 'hugoresetpwtwice@hotmail.com' });
+    response = await request(app)
+      .post(`/api/users/password/reset/${user.resetPasswordToken}`)
+      .send({
+        password: 'ABCDEFGH',
+      });
+    expect(response.status).toBe(200);
+
+    response = await request(app)
+      .post('/api/users/login')
+      .send({
+        email: 'hugoresetpwtwice@hotmail.com',
+        password: 'ABCDEFGH',
+      });
+
+    expect(response.status).toBe(200);
+
+    response = await request(app)
+      .get(`/api/users/password/reset/${user.resetPasswordToken}`);
+    
+    expect(response.status).toBe(403);
+    expect(response.body.errorCode).toBe(errors.TOKEN_EXPIRED);
+
+    done();
+  });
 });
