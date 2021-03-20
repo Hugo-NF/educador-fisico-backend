34b1bab5a3618ffa3c5d9e713c968052e5436cc5
ailamaralves <ailamar.alvesg@gmail.com>
2021-03-19 21:42:44 -0300

    Update edith method

--- src/controllers/AccountController.js
+++ src/controllers/AccountController.js
@@ -10,11 +10,8 @@ module.exports = {
     logger.info(`Request origin: ${request.ip}`);
     logger.info('Inbound request to /users/login');
 
-		const { id };
-    const {
-      name, email, birthDate, sex, phone, city, state,
-    } = request.body;
-
+		const { id } = request.params;
+    
     try {
       const user = await User.findById(id);
       if (user == null) {
@@ -30,13 +27,15 @@ module.exports = {
       return response.json({
         statusCode: 200,
         data: {
-          name, 
-					email,  
-					birthDate, 
-					sex, 
-					phone, 
-					city, 
-					state,
+          user : {
+            name = user.name, 
+            email = user.email,  
+            birthDate = user.birthDate, 
+            sex = user.sex, 
+            phone = user.phone, 
+            city = user.city, 
+            state = user.state,
+          }
         },
       });
     } catch (exc) {
@@ -52,41 +51,79 @@ module.exports = {
 
   // Edit method
   async edit(request, response) {
-    logger.info(`Request origin: ${request.ip}`);
-    logger.info('Inbound request to /exercise/edit');
+    logger.info(`Request origin: ${request.id}`);
+    logger.info('Inbound request to /user/login');
 
+    const { id } = request.params;
     const {
       name, email, birthDate, sex, phone, city, state,
     } = request.body;
 
     try {
-      const user = await User.findByIdAndUpdate(email, { name, email, birthDate, sex, phone, 
+      const user = await User.findByIdAndUpdate(id, { name, email, birthDate, sex, phone, 
 			city, state, }, { new: true });
       if (user == null) {
-        logger.error(`/exercise/edit: Could not find exercise with ObjectId(${id})`);
+        logger.error(`/user/login: Could not find iser profile (${id})`);
 
         return response.status(404).json({
           statusCode: 404,
           errorCode: errors.RESOURCE_NOT_IN_DATABASE,
-          message: 'Exercise could not be found',
+          message: 'Profile could not be found',
         });
       }
 
-      logger.info(`Request to /exercise/edit successfully edited ObjectId(${id})`);
+      logger.info(`Request to /user/login successfully logged (${id})`);
       return response.json({
         statusCode: 200,
         data: {
-          exercise,
+          user : {
+            name = user.name, 
+            email = user.email,  
+            birthDate = user.birthDate, 
+            sex = user.sex, 
+            phone = user.phone, 
+            city = user.city, 
+            state = user.state,
+          }
         },
       });
     } catch (exc) {
-      logger.error(`Error while running /exercise/edit. Details: ${exc}`);
+      logger.error(`Error while running /user/login. Details: ${exc}`);
       return response.status(500).json({
         statusCode: 500,
         errorCode: errors.UNKNOWN_ERROR,
-        message: `Could not edit exercise with ${id}`,
+        message: `Could not open profile ${id}`,
         error: exc,
       });
     }
-  },
+
+    try {
+      await user.updateOne({
+        emailConfirmationTokenExpiration: currentUTC,
+        emailConfirmed: false,
+      });
+    }
+
+    // TODO: Replace e-mail info with real data as soon as available
+
+  // Dispatch e-mail
+  /* mailer.sendEmails([{ Email: email, Name: user.name }], 'Confirm Your Email', content, sandboxMode)
+    .then(() => {
+      logger.info(`E-mail with account activation token sent successfully to account (${email})`);
+
+      return response.json({
+        statusCode: 200,
+        message: 'E-mail updated successfully',
+      });
+    })
+    .catch((error) => {
+      logger.error(`E-mail with account activation token failed to send to (${email}). Details: ${error}`);
+
+      return response.status(503).json({
+        statusCode: 503,
+        errorCode: errors.MAILJET_UNAVAILABLE,
+        message: 'Could not update e-mail',
+      });
+    });
+  }, */
 };
